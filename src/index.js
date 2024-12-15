67bf895e48e36e7aee04418496442d02e64952b3
sagorsohel <sohel19sagor@gmail.com>
2024-12-16 01:41:45 +0600

    add app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting", err);
+  });
