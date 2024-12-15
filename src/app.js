af52aa8e2835726ba443f536d6edbe38d17f644e
sagorsohel <sohel19sagor@gmail.com>
2024-12-16 01:53:27 +0600

    add cors in app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    // optionsSuccessStatus: 200,
+    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    // exposedHeaders: ['Set-Cookie']
+  })
+);
 
 export { app };
