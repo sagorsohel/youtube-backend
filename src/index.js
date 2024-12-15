0747a77c5359929a9e3a14a058aa78a620cb50e7
sagorsohel <sohel19sagor@gmail.com>
2024-12-15 23:58:12 +0600

    connect database

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import mongoose from "mongoose";
-import express from "express";
-const app = express();
+import dotenv from 'dotenv'
+import connectDb from "./db/index.js";
 
-// Connect to MongoDB
-(async () => {
-  try {
-    mongoose.connect(`${process.env.MONGODB_URL}/youtube`);
-    app.on("error", (error) => {
-      console.log("Error connecting", error);
-      throw error;
-    });
-    app.listen(`${process.env.PORT}`, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.log("error connecting to MongoDB", error);
-    throw error;
-  }
-})();
+dotenv.config({
+    path:'./env'
+})
+
+connectDb()
